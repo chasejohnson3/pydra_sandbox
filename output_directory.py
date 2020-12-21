7a52689da0d9b39dbfad785ad20048b01a890856
Chase Johnson <chaser.johnson27@gmail.com>
2020-12-21 14:27:49 -0600

    Created a file to demonstrate 'file does not exist' error

---
+++ output_directory.py
@@ -0,0 +1,48 @@
+import nest_asyncio
+nest_asyncio.apply()
+import pydra
+import attr
+
+cmd = "echo"
+
+my_input_spec = pydra.specs.SpecInfo(
+        name="Input",
+        fields=[
+            (
+                "outputFile",
+                attr.ib(
+                    type=pydra.specs.Directory,
+                    metadata={"help_string": "The name of a file to be created.", "mandatory": True},
+                ),
+            )
+        ],
+        bases=(pydra.specs.ShellSpec,),
+    )
+
+my_output_spec = pydra.specs.SpecInfo(
+        name="Output",
+        fields=[
+            (
+                "outputFile",
+                attr.ib(
+                    type=pydra.specs.File,
+                    metadata={
+                        "help_string": "The output file",
+                        "output_file_template": "{outputFile}"
+                    },
+                ),
+            ),
+        ],
+        bases=(pydra.specs.ShellOutSpec,),
+    )
+
+
+
+shelly = pydra.ShellCommandTask(name="shelly", executable=cmd, input_spec=my_input_spec, output_spec=my_output_spec)
+shelly.inputs.outputFile = "out_file.txt"
+
+print("cmndline = ", shelly.cmdline)
+
+with pydra.Submitter(plugin="cf") as sub:
+    sub(shelly)
+shelly.result()
